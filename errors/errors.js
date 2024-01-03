7803deaabfc96966a1ed4d6f3b66aa1fe2d05eba
Beatriz <beatriz.pardington@gmail.com>
2024-01-03 16:11:43 +0000

    added mongo cutom errors

--- errors/errors.js
+++ errors/errors.js
@@ -1,3 +1,12 @@
+exports.handleMongoErrors = (err, req, res, next) => {
+  if (err.errors) {
+    const reason = Object.keys(err.errors)[0]
+    res.status(400).send({msg: err.errors[reason].properties.message})
+  }else{
+    next(err)
+  }
+}
+
 exports.handleCustomErrors = (err, req, res, next) => {
   if (err.status) {
     res.status(err.status).send({ msg: err.msg });
