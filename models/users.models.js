101d08257366bbcee1c8a7a2e0a293a916468dc3
AydaFox <burrhe.y02@gmail.com>
2024-01-03 09:35:48 +0000

    refactors async functions to promises

--- models/users.models.js
+++ models/users.models.js
@@ -1,14 +1,15 @@
 const User = require("../db/schema/user-schema");
 
-exports.selectUserById = async (id) => {
+exports.selectUserById = (id) => {
   if (id.length !== 24) {
     return Promise.reject({ status: 400, msg: "bad request" });
   }
 
-  const user = await User.findById(id);
-  if (user) {
-    return user;
-  } else {
-    return Promise.reject({ status: 404, msg: "user not found" });
-  }
+  return User.findById(id).then((user) => {
+    if (user) {
+      return user;
+    } else {
+      return Promise.reject({ status: 404, msg: "user not found" });
+    }
+  });
 };
